5c739c4db9abff3e64f4580902d2e38a6b669215
root <root@pop-os.localdomain>
2021-01-30 18:19:39 -0300

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,8 +2,8 @@
 @section('main')
 
     <div class="text-center">
-        <a href="/" class="w-25 btn btn-sm btn-primary">Generator</a>
-        <a href="/history" class="w-25  btn btn-sm btn-light">History</a>
+        <a href="/" class=" btn btn-sm btn-primary">Generator</a>
+        <a href="/history" class="  btn btn-sm btn-light">History</a>
     </div>
     <h4 class="text-center font-weight-bold text-uppercase mb-0 py-2">{{ env('APP_NAME') }} - Votemap Generator</h4>
 
